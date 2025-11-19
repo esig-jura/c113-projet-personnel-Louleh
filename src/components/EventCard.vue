25af4830b94ede3ce55c2e6cb0eb3e4911c30459
lehmannl <lehmann.lou@bluewin.ch>
2025-11-19 13:24:32 +0100

    Add events page

---
+++ src/components/EventCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card__container">
+    <div>
+      <h2>{{ label }}</h2>
+    </div>
+    <div class="container__body">
+      <img :src="img" alt="" />
+    </div>
+    <div>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  img: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.card__container {
+  border: 1px solid var(--quaternary);
+  border-radius: 8px;
+  background-color: var(--secondary);
+  color: var(--quaternary);
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  width: 70%;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+
+.container__body img {
+  width: 100%;
+  display: block;
+  object-fit: cover;
+}
+</style>
---
+++ src/views/Events.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <tile v-for="event in events" :label="event.Title" :description="event.Description" :img="event.Flyer"></tile>
+  </div>
+</template>
+
+<script setup>
+import Tile from '@/components/EventCard.vue'
+
+import img1 from '../assets/images/welcomePicture.png'
+import img2 from '../assets/images/gentlemens_1.png'
+
+const events = [
+  {
+    Title: "Event 1",
+    Description: "Description for Event 1",
+    Flyer: img1,
+  },
+  {
+    Title: "Event 2",
+    Description: "Description for Event 2",
+    Flyer: img2,
+  }
+]
+
+</script>
+
+<style scoped>
+
+.container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  padding: 16px;
+}
+</style>
