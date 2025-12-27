d2ad616c0a60c694517b8cfc57827bfe4701e111
lehmannl <lehmann.lou@bluewin.ch>
2025-12-27 16:08:03 +0100

    Fix rename

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Header from "@/components/header.vue";
-import Footer from "@/components/footer.vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import Welcome from '@/views/welcome.vue'
+import Welcome from '@/views/Welcome.vue'
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
